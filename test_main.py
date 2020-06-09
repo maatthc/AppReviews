846fa7a84db59c0f9653d65668a34ea52338e136
Alexandre Andrade <maatthc@users.noreply.github.com>
2020-06-10 00:24:30 +1000

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,3 +1,5 @@
+expected = 'Semaphore'
+
 def capital_case(x):
     return x.capitalize()
 
@@ -5,7 +7,7 @@ def capital_case_wrong(x):
     return x
 
 def test_capital_case():
-    assert capital_case('semaphore') == 'Semaphore'
+    assert capital_case('semaphore') == expected
 
 def test_capital_case_wrong():
-    assert capital_case_wrong('semaphore') != 'Semaphore'
+    assert capital_case_wrong('semaphore') != expected
