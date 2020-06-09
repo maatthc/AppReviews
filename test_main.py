8d53632ffd871ef4e54df41bdf02f3eab20c81a7
Alexandre Andrade <maatthc@users.noreply.github.com>
2020-06-10 00:18:57 +1000

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,9 +1,11 @@
 def capital_case(x):
     return x.capitalize()
 
+def capital_case_wrong(x):
+    return x
 
 def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
 
 def test_capital_case_wrong():
-    assert capital_case('semaphore') != 'Semaphore'
+    assert capital_case_wrong('semaphore') != 'Semaphore'
