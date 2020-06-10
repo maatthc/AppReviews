dacba08130090974d1ea8198233affafff715f62
Alexandre Andrade <maatthc@users.noreply.github.com>
2020-06-10 16:54:32 +1000

    Update test_main.py (#3)
    * Update test_main.py
    
    * Update test_main.py
    
    * Update test_main.py
    
    Co-authored-by: Alexandre Andrade Silva <alexandre.silva@kmart.com.au>

--- test_main.py
+++ test_main.py
@@ -1,6 +1,18 @@
+expected = 'Semaphore'
+
 def capital_case(x):
     return x.capitalize()
 
+def capital_case_wrong(x):
+    return x
 
 def test_capital_case():
-    assert capital_case('semaphore') == 'Semaphore'
+    assert capital_case('semaphore') == expected
+
+def test_capital_case_wrong():
+    assert capital_case_wrong('semaphore') != expected
+
+def breaking_test():
+    expected = 'MaaT'
+    assert capital_case_wrong('maat') != expected
+
